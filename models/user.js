fc70a9990339e197459c8c64767a1bcee220e7b8
Harshit Srivastava <harshitcsrivastava@gmail.com>
2022-08-08 17:30:32 +0530

    Add Project API work done

--- models/user.js
+++ models/user.js
@@ -1,22 +1,31 @@
 const mongoose = require('mongoose');
-const UserSchema  = new mongoose.Schema({
-  name :{
-      type  : String,
-      required : true
-  } ,
-  email :{
-    type  : String,
-    required : true
-} ,
-password :{
-    type  : String,
-    required : true
-} ,
-date :{
-    type : Date,
-    default : Date.now
-}
+const UserSchema = new mongoose.Schema({
+    projectName: {
+        type: String,
+        required: true
+    },
+    projectLeadName: {
+        type: String,
+        required: true
+    },
+    projectType: {
+        type: String,
+        required: true,
+        default: 'project'
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    }
 });
-const User= mongoose.model('User',UserSchema);
+const User = mongoose.model('User', UserSchema);
 
 module.exports = User;
--- routes/index.js
+++ routes/index.js
@@ -6,18 +6,21 @@ router.get('/', (req,res)=>{
     res.render('welcome');
 })
 //register page
-router.get('/register', (req,res)=>{
-    res.render('register');
+router.get('/addProject', (req,res)=>{
+    res.render('addProject');
 })
 router.get('/dashboard',ensureAuthenticated,(req,res)=>{
     res.render('dashboard',{
         user: req.user
     });
 })
-
 router.get('/addProject', ensureAuthenticated,(req,res)=>{
     res.render('AddProject',{
         user: req.user
     });
 });
+
+router.get('/getProjects', (req,res)=>{
+    res.render('getProjects');
+})
 module.exports = router; 
