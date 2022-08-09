c514b16880214da4c7b222beb3579e6104c77fd2
Harshit Srivastava <harshitcsrivastava@gmail.com>
2022-08-09 12:26:11 +0530

    addUser

--- public/CustomeJS/Custome.js
+++ public/CustomeJS/Custome.js
@@ -11,6 +11,11 @@ $(document).ready(function(){
         saveProject();
     })
 
+    $('.AddUser').on('submit', (e)=>{
+        e.preventDefault();
+        saveUser();
+    })
+
 });
 
 async function saveProject(){
@@ -24,6 +29,37 @@ async function saveProject(){
 
         console.log(data)
         const URL = baseUrl + 'users/saveProject';
+        const options = {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                "content-type": "application/json"
+            }
+        }
+        let response = await fetch(URL, options);
+        response = await response.json();
+        console.log(response)
+        if(response.status !== 200){
+            $('.formMsg').text(response.message)
+        }else{
+            $('.formMsg').text(response.message)
+        }
+    }catch(error){
+        console.log(error)
+    }
+}
+
+async function saveUser(){
+    try{
+        const dataArr = $('.AddUser').serializeArray();
+
+        let data = {};
+        for(key of dataArr){
+            data[key.name] = key.value;
+        }
+
+        console.log(data)
+        const URL = baseUrl + 'users/saveUser';
         const options = {
             method: 'POST',
             body: JSON.stringify(data),
