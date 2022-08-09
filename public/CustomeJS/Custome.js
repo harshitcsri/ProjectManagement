e77b48834325f5cdd6b903e3d8480827eb9f4a22
SVish9050 <shivam1@quadrafort.com>
2022-08-09 15:56:58 +0530

    Add Project Detail Function

--- public/CustomeJS/Custome.js
+++ public/CustomeJS/Custome.js
@@ -14,12 +14,6 @@ $(document).ready(function () {
 
     getAllProjects();
 
-    const id = window.location.href;
-    const projectId = id.split('=')[1];
-    // console.log(projectId)
-    console.log(projectId)
-    getSingleProject(projectId);
-
 });
 
 $(document).ajaxComplete(function(){
@@ -132,17 +126,5 @@ async function saveUser(){
         }
     }catch(error){
         console.log(error)
-    }
-}
-
-async function getSingleProject(id){
-    console.log(id)
-    const URL = baseUrl + 'users/getProject?_id='+id;
-    let response = await fetch(URL);
-    response = await response.json();
-    console.log(response);
-    if(response.status == 200){
-        $('#projectName').text(response.data[0].ProjectName)
-        $('#projectLead').text(response.data[0].ProjectLead)
     }
 }
