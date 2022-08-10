25f774ef03f53ebb4ca4332cb2d97f3eb6ba2707
Harshit Srivastava <harshitcsrivastava@gmail.com>
2022-08-10 10:46:08 +0530

    updated

--- routes/index.js
+++ routes/index.js
@@ -1,42 +1,42 @@
 const express = require('express');
-const router  = express.Router();
-const {ensureAuthenticated} = require('../config/auth') 
+const router = express.Router();
+const { ensureAuthenticated } = require('../config/auth')
 //login page
-router.get('/', (req,res)=>{
+router.get('/', (req, res) => {
     res.render('welcome');
 })
 //register page
-router.get('/register', (req,res)=>{
+router.get('/register', (req, res) => {
     res.render('register');
 })
-router.get('/dashboard',ensureAuthenticated,(req,res)=>{
-    res.render('dashboard',{
+router.get('/dashboard', ensureAuthenticated, (req, res) => {
+    res.render('dashboard', {
         user: req.user
     });
 })
 
-router.get('/addProject', ensureAuthenticated,(req,res)=>{
-    res.render('AddProject',{
+router.get('/addProject', ensureAuthenticated, (req, res) => {
+    res.render('AddProject', {
         user: req.user
     });
 });
 
 
-router.get('/manageProject', ensureAuthenticated,(req,res)=>{
-    res.render('ManageProjects',{
+router.get('/manageProject', ensureAuthenticated, (req, res) => {
+    res.render('ManageProjects', {
         user: req.user
     });
 });
 
-router.get('/projectDetail', ensureAuthenticated,(req,res)=>{
-    res.render('ProjectDetails',{
+router.get('/projectDetail', ensureAuthenticated, (req, res) => {
+    res.render('ProjectDetails', {
         user: req.user
     });
 });
 
-router.get('/addUser',ensureAuthenticated,(req,res)=>{
-    res.render('AddUser',{
-        user:req.user
+router.get('/addUser', ensureAuthenticated, (req, res) => {
+    res.render('AddUser', {
+        user: req.user
     })
 });
 
